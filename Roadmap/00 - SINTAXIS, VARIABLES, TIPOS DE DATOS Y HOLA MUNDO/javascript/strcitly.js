b0547b7213c54039bbbae6411eace7af5752df7a
strcitly <alvaroandnin@gmail.com>
2025-12-29 19:02:34 +0100

    00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/strcitly.js
@@ -0,0 +1,16 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// Este es un comentario de una linea
+
+/* Este
+es 
+un 
+comentario
+de varias
+lineas */
+
+const nombre = "alvaro"
+let edad = 24
+var novias = 1
+
+console.log("Hola, JavaScript")
